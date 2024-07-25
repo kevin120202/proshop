b3c20cb0e0ac4c9cfa22760b511037f124566786
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-25 14:57:45 -0500

    added redux base starter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
+import { Provider } from "react-redux"
+import store from './store.js'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,6 +20,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>,
 )
